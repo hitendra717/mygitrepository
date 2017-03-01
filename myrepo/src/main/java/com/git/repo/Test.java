601f512decf720d1c3e4c60fbaeb7ef1bced11fd
Hitendra Yadav <hitendra.yadav005@gmail.com>
2017-03-01 18:30:36 +0530

    Test java file

---
+++ myrepo/src/main/java/com/git/repo/Test.java
@@ -0,0 +1,14 @@
+package com.git.repo;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("First Git Tutorial Project.");
+
+	}
+
+}
